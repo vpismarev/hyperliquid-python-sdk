ebdd5b6c711a1438d4cda35db701240c608dc833
vitaly.pismarev <vitaly.pismarev@exness.com>
2025-05-05 20:29:40 +0200

    new commit

--- basic_ws.py
+++ basic_ws.py
@@ -1,72 +1,41 @@
 import json
-from datetime import datetime
-
-import boto3
-from botocore.exceptions import NoCredentialsError
+import logging
+import socket
 
 import example_utils
-
 from hyperliquid.utils import constants
 
-
-class S3:
-    def __init__(self, aws_access_key_id, aws_secret_access_key):
-        self.s3_client = boto3.client(
-            's3',
-            aws_access_key_id=aws_access_key_id,
-            aws_secret_access_key=aws_secret_access_key
-        )
-
-    def save_json_to_s3(self, json_data, bucket_name, s3_file_path):
-        try:
-            json_string = json.dumps(json_data)
-
-            # Загружаем JSON строку в S3
-            self.s3_client.put_object(
-                Bucket=bucket_name,
-                Key=s3_file_path,
-                Body=json_string,
-                ContentType='application/json'
-            )
-            print(f"JSON данные успешно сохранены в {bucket_name}/{s3_file_path}")
-            return True
-        except NoCredentialsError:
-            print("Учетные данные AWS не предоставлены или неверны")
-            return False
-        except Exception as e:
-            print(f"Произошла ошибка при сохранении JSON: {e}")
-            return False
+C_HOST = 'localhost'
+C_PORT = 8686
 
 
-class LP(S3):
-    def __init__(self, aws_access_key_id, aws_secret_access_key, coin):
-
-        super().__init__(aws_access_key_id, aws_secret_access_key)
+class LP:
+    def __init__(self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((C_HOST, C_PORT))
         self.address, self.info, _ = example_utils.setup(constants.MAINNET_API_URL)
-        self.coin = coin
 
-        self.batch = []
-        self.batch_size = 100
-        self.idx = 1
-
-    def subscribe(self):
-        self.info.subscribe({"type": "l2Book", "coin": self.coin}, self.save_result)
+    def subscribe(self, coin):
+        self.info.subscribe({"type": "l2Book", "coin": coin}, self.save_result)
 
     def save_result(self, response: str):
-        if self.idx < self.batch_size:
-            self.batch.append(response)
-            self.idx += 1
-        else:
-            self.save_json_to_s3(json_data=self.batch,
-                                 bucket_name='pvv-crypto-storage',
-                                 s3_file_path=f'data/{datetime.now().strftime("%Y%m%d")}/{self.coin.replace("/","")}_{datetime.now().strftime("%Y%m%d%H%M%S%f")}.json')
-            self.batch = []
-            self.idx = 1
+        msg = json.dumps(response) + chr(10)
+        self.socket.send(msg.encode())
+
+
+def setup_logging():
+    """Set up logging configuration"""
+    logging.basicConfig(
+        level=logging.INFO,
+        filename='hyperliquid.log',
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
 
 
 if __name__ == "__main__":
-    # main()
-    lp = LP(aws_access_key_id='aaa',
-            aws_secret_access_key='bbb',
-            coin="UETH/USDC")
-    lp.subscribe()
+    setup_logging()
+    lp = LP()
+    lp.subscribe(coin="ETH")
+    lp.subscribe(coin="BTC")
+    lp.subscribe(coin="SOL")
