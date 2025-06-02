0a00dceb2ddf29d4d983a26226faf5216750f10d
vitaly.pismarev <vitaly.pismarev@exness.com>
2025-06-02 12:56:51 +0200

    new commit

--- meteora.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-header = {'accept': 'application/json'}
-data = requests.get('https://dlmm-api.meteora.ag/info/protocol_metrics', headers=header)
-
-
-print(data.content.decode())
